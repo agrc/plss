784457e56c52b13cfb34999a54661842d5e43293
steveoh <sgourley@utah.gov>
2014-03-26 17:31:05 -0600

    migration for user token

---
+++ PLSS/Migrations/Migration2.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace PLSS.Migrations
+{
+    [Migration(2, "add token to user column")]
+    public class Migration2 : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("Users").InSchema("PLSSADMIN").AddColumn("Token").AsString(128).Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("Users").FromTable("Coordinates").InSchema("PLSSADMIN");
+        }
+    }
+}
