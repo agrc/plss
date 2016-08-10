384c0b8edb6b08acdbaa614f0431f12676601bf7
steveoh <sgourley@utah.gov>
2016-08-09 18:33:55 -0600

    Release v2.0.0

--- PLSS/Properties/AssemblyInfo.cs
+++ PLSS/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.5.0715")]
-[assembly: AssemblyFileVersion("0.1.5.0715")]
+[assembly: AssemblyVersion("2.0.0.0000")]
+[assembly: AssemblyFileVersion("2.0.0.0000")]
