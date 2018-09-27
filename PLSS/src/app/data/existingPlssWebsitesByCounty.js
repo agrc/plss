6275a0e4a651f0ce8d82446b4e3cdd611fbc8c99
steveoh <sgourley@utah.gov>
2018-09-27 09:44:53 -0600

    fix: update county url
    closes #22

--- PLSS/src/app/data/existingPlssWebsitesByCounty.js
+++ PLSS/src/app/data/existingPlssWebsitesByCounty.js
@@ -1,11 +1,9 @@
-﻿define({
-    'UTAH': 'http://maps2.utahcountyonline.org/TieSheets/TieSheet.htm',
-    'WASATCH': 'http://www.co.wasatch.ut.us/departments/' +
-                'property_tax_services/surveyor/on-line_survey_lookup_service.aspx',
+define({
+    'UTAH': 'https://maps.utahcounty.gov/TieSheets/TieSheet.htm',
+    'WASATCH': 'https://wasatch.maps.arcgis.com/apps/webappviewer/index.html?id=103db0251a5342f7bbd1462eb7a47440',
     'DAVIS': 'http://www.co.davis.ut.us/surveyor/default.cfm',
-    'SALT LAKE': 'http://surveyor.slco.org/jsapi/map/survey_monument.html',
-    'WEBER': 'http://www.co.weber.ut.us/gis/maps/gizmo/',
-    'DUCHESNE': 'http://duchesne.utah.gov/surveyor/base&meridian.html',
-    'UINTAH': 'http://www.co.uintah.ut.us/surveyor/surveyor.php',
-    'SAN JUAN': 'http://www.sanjuancounty.org/surveyor_rosp_index.htm'
+    'SALT LAKE': 'https://slco.org/surveyor/apps/surveymonument/map.html',
+    'WEBER': 'https://www.co.weber.ut.us/gis/maps/gizmo/',
+    'DUCHESNE': 'https://duchesne.utah.gov/surveyor/base&meridian.html',
+    'UINTAH': 'https://www.co.uintah.ut.us/surveyor/UCTieSheet.htm'
 });
