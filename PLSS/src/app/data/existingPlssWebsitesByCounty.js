efcb20be3e3d3ca244d2648df2606fb7ef655d6a
steveoh <sgourley@utah.gov>
2019-02-28 09:18:16 -0700

    fix: add index.html because of directory browsing

--- PLSS/src/app/data/existingPlssWebsitesByCounty.js
+++ PLSS/src/app/data/existingPlssWebsitesByCounty.js
@@ -3,7 +3,7 @@ define({
     'WASATCH': 'https://wasatch.maps.arcgis.com/apps/webappviewer/index.html?id=103db0251a5342f7bbd1462eb7a47440',
     'DAVIS': 'http://www.co.davis.ut.us/surveyor/default.cfm',
     'SALT LAKE': 'https://slco.org/surveyor/apps/surveymonument/map.html',
-    'WEBER': 'https://www3.co.weber.ut.us/gis/maps/survey/',
+    'WEBER': 'https://www3.co.weber.ut.us/gis/maps/survey/index.html',
     'DUCHESNE': 'https://duchesne.utah.gov/surveyor/base&meridian.html',
     'UINTAH': 'https://www.co.uintah.ut.us/surveyor/UCTieSheet.htm'
 });
