ec899d6c369f6a6530061e23f764f40b29de4404
steveoh <sgourley@utah.gov>
2021-02-10 15:00:43 -0700

    style(app): format map, update css, and remove padding

--- src/plss.web/plss-client/src/components/Map/index.js
+++ src/plss.web/plss-client/src/components/Map/index.js
@@ -1,16 +1,17 @@
-import React, { useEffect, useState, useRef } from 'react';
-import EsriMap from '@arcgis/core/Map';
-import MapView from '@arcgis/core/views/MapView';
+import LayerSelector from '@agrc/layer-selector';
 import Basemap from '@arcgis/core/Basemap';
+import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
 import LOD from '@arcgis/core/layers/support/LOD';
 import TileInfo from '@arcgis/core/layers/support/TileInfo';
-import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
 import WebTileLayer from '@arcgis/core/layers/WebTileLayer';
-import LayerSelector from '@agrc/layer-selector';
+import EsriMap from '@arcgis/core/Map';
+import MapView from '@arcgis/core/views/MapView';
+import React, { useEffect, useRef, useState } from 'react';
 
 const urls = {
-  landownership: 'https://gis.trustlands.utah.gov/server/' +
-    '/rest/services/Ownership/UT_SITLA_Ownership_LandOwnership_WM/FeatureServer/0'
+  landownership:
+    'https://gis.trustlands.utah.gov/server/' +
+    '/rest/services/Ownership/UT_SITLA_Ownership_LandOwnership_WM/FeatureServer/0',
 };
 
 export default function PlssMap() {
@@ -23,44 +24,44 @@ export default function PlssMap() {
     }
 
     const esriMap = new EsriMap({
-      basemap: {}
+      basemap: {},
     });
     const mapView = new MapView({
       container: node.current,
       map: esriMap,
-      padding: {
-        left: 60
-      },
       extent: {
         xmax: -11762120.612131765,
         xmin: -13074391.513731329,
         ymax: 5225035.106177688,
         ymin: 4373832.359194187,
-        spatialReference: 3857
+        spatialReference: 3857,
       },
       ui: {
-        components: ['zoom']
-      }
+        components: ['zoom'],
+      },
     });
 
     setSelectorOptions({
       view: mapView,
       quadWord: process.env.REACT_APP_DISCOVER,
       baseLayers: ['Hybrid', 'Lite', 'Terrain', 'Topo', 'Color IR'],
-      overlays: ['Address Points', {
-        Factory: FeatureLayer,
-        url: urls.landownership,
-        id: 'Land Ownership',
-        opacity: 0.3
-      }],
+      overlays: [
+        'Address Points',
+        {
+          Factory: FeatureLayer,
+          url: urls.landownership,
+          id: 'Land Ownership',
+          opacity: 0.3,
+        },
+      ],
       modules: { LOD, TileInfo, Basemap, WebTileLayer, FeatureLayer },
-      position: 'top-right'
+      position: 'top-right',
     });
   }, []);
 
   return (
-    <div ref={node} className="w-screen h-screen bg-white">
-      { selectorOptions ? <LayerSelector {...selectorOptions}></LayerSelector> : null}
+    <div ref={node} className="bg-white parole__map">
+      {selectorOptions ? <LayerSelector {...selectorOptions}></LayerSelector> : null}
     </div>
   );
 }
