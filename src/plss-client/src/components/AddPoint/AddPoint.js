ec9dc72b350338d973eb4677de26efd12f5b18c0
steveoh <sgourley@utah.gov>
2021-02-18 20:54:11 -0700

    feat(add-point): mock adding images

--- src/plss-client/src/components/AddPoint/AddPoint.js
+++ src/plss-client/src/components/AddPoint/AddPoint.js
@@ -3,10 +3,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { contrastColor } from 'contrast-color';
 import * as React from 'react';
 import { CirclePicker } from 'react-color';
+import { useImmer } from 'use-immer';
 
 const numberFormatter = new Intl.NumberFormat('en-US');
 
-export default function AddPoint({ active, color, point, dispatch, notes }) {
+export default function AddPoint({ active, color, point, dispatch, notes, photos = [] }) {
+  const [images, updateImage] = useImmer(photos);
+  const addImage = () => {
+    updateImage((draft) => {
+      draft.push(0);
+    });
+  };
+
   const inactiveStyle = {
     color: color.hex,
     backgroundColor: '#111',
@@ -25,17 +33,17 @@ export default function AddPoint({ active, color, point, dispatch, notes }) {
       <h1 className="text-2xl font-bold">Add Point</h1>
       <section className="inline-grid gap-3 mt-3">
         <div>
-          <div className="flex w-full">
+          <div className="flex">
             <button
               style={active ? style : inactiveStyle}
-              className="px-6 font-bold text-white transition duration-100 ease-in-out border-t border-b border-l border-gray-600 rounded-l-lg"
+              className="px-6 font-bold text-white transition duration-100 ease-in-out border-t border-b border-l border-gray-600 rounded-l-lg focus:outline-none"
               type="button"
               onClick={() => dispatch({ type: 'add-point/activate' })}
             >
               {active ? 'draw' : 'activate'}
             </button>
             <input
-              className="flex-1 px-3 py-2 text-gray-800 bg-white border-t border-b border-r border-gray-500 rounded-r-lg focus:border-indigo-500 focus:ring-2 focus:ring-indigo-500 focus:outline-none focus:ring-opacity-50"
+              className="flex-1 w-full px-3 py-2 text-gray-800 bg-white border-t border-b border-r border-gray-500 rounded-r-lg focus:border-indigo-500 focus:ring-2 focus:ring-indigo-500 focus:outline-none focus:ring-opacity-50"
               type="text"
               placeholder="Point Name"
             />
@@ -71,10 +79,25 @@ export default function AddPoint({ active, color, point, dispatch, notes }) {
           }
         />
 
-        <h2 className="text-lg font-bold">Photos</h2>
+        <div className="flex flex-row justify-between">
+          <h2 className="text-lg font-bold">Photos</h2>
+          <button
+            type="button"
+            className="self-center px-3 py-1 text-xs uppercase transition bg-transparent border border-white rounded w-min ripple hover:bg-indigo-300 hover:text-black focus:outline-none whitespace-nowrap"
+            onClick={addImage}
+          >
+            + Add
+          </button>
+        </div>
+
+        <div className="flex flex-wrap self-center gap-3 justify-evenly">
+          {images.map(() => (
+            <div className="w-20 h-20 bg-gray-400 border border-gray-800 rounded"></div>
+          ))}
+        </div>
         <div className="flex items-end justify-center">
           <button
-            className="inline-block px-6 py-2 text-xs font-medium leading-6 text-center text-white uppercase transition bg-transparent border-2 border-green-500 rounded disabled:border-red-500 disabled:cursor-not-allowed disabled:opacity-50 w-min ripple hover:bg-green-300 hover:text-black focus:outline-none"
+            className="inline-block px-6 py-2 text-xs font-medium text-center text-white uppercase transition bg-transparent border-2 border-green-500 rounded disabled:border-red-500 disabled:cursor-not-allowed disabled:opacity-50 w-min ripple hover:bg-green-300 hover:text-black focus:outline-none"
             type="button"
             disabled={!point}
           >
