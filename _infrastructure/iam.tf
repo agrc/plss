ea61f6fe46203c6b267d7e88d8c015aacf3f37e8
steveoh <sgourley@utah.gov>
2022-06-22 20:05:40 -0600

    refactor: out with the old

--- _infrastructure/iam.tf
+++
@@ -1,7 +0,0 @@
-// requires policy exception
-
-resource "google_storage_bucket_iam_member" "public" {
-  bucket = google_storage_bucket.approved.name
-  member = "allUsers"
-  role   = "roles/storage.objectViewer"
-}
