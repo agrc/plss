8e9a59ed8ee245ef7400c65b023f4a98ba54a1ab
steveoh <sgourley@utah.gov>
2022-09-15 14:49:53 -0600

    chore: add deps to types

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,21 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-enum': [2, 'always', ['api', 'app']],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'refactor',
+        'deps',
+        'style',
+        'test',
+      ],
+    ],
   },
 };
