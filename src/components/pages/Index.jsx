53aa7124cac75400ef4d5cd6a349c7a5291f6db9
steveoh <sgourley@utah.gov>
2022-09-27 13:59:55 -0600

    feat: create basic image uploading
    refs #93

--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -1,6 +1,7 @@
 import { useImmerReducer } from 'use-immer';
 import { getFunctions, connectFunctionsEmulator } from 'firebase/functions';
-import { useFirebaseApp, FunctionsProvider } from 'reactfire';
+import { getStorage, connectStorageEmulator } from 'firebase/storage';
+import { useFirebaseApp, FunctionsProvider, StorageProvider } from 'reactfire';
 import reduce, { defaults } from '../reducers/AppReducer';
 import Drawer from '../layoutElements/Drawer.jsx';
 import Menu from '../layoutElements/Menu.jsx';
@@ -10,6 +11,7 @@ export default function Index() {
   const [state, dispatch] = useImmerReducer(reduce, defaults);
   const app = useFirebaseApp();
   const functions = getFunctions(app);
+  const storage = getStorage(app);
 
   if (import.meta.env.DEV) {
     if (
@@ -22,16 +24,36 @@ export default function Index() {
         console.log('functions emulator already connected');
       }
       if (typeof window !== 'undefined') {
-        window['_firebase_function'] = true;
+        window['_firebase_function_emulator'] = true;
+      }
+    }
+
+    if (
+      typeof window === 'undefined' ||
+      !window['_firebase_storage_emulator']
+    ) {
+      try {
+        connectStorageEmulator(storage, 'localhost', 9199);
+      } catch {
+        console.log('storage emulator already connected');
+      }
+      if (typeof window !== 'undefined') {
+        window['_firebase_storage_emulator'] = true;
       }
     }
   }
 
   return (
     <FunctionsProvider sdk={functions}>
-      <Map state={state.map} color={state.addPoint.color} dispatch={dispatch} />
-      <Menu>menu</Menu>
-      <Drawer {...state} dispatch={dispatch} />
+      <StorageProvider sdk={storage}>
+        <Map
+          state={state.map}
+          color={state.addPoint.color}
+          dispatch={dispatch}
+        />
+        <Menu>menu</Menu>
+        <Drawer {...state} dispatch={dispatch} />
+      </StorageProvider>
     </FunctionsProvider>
   );
 }
