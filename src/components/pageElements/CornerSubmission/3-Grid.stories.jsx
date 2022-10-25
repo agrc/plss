5c71898b8b6b9ec639492ce5182f521b75db5e76
steveoh <sgourley@utah.gov>
2022-10-25 13:12:05 -0600

    test: create and organize all stories

---
+++ src/components/pageElements/CornerSubmission/3-Grid.stories.jsx
@@ -0,0 +1,49 @@
+import GridCoordinates from './GridCoordinates.jsx';
+import { SubmissionProvider } from '../../contexts/SubmissionContext.jsx';
+
+export default {
+  title: 'Corner/Submission/Parts',
+  component: GridCoordinates,
+  decorators: [
+    (Story) => (
+      <SubmissionProvider context={{ blmPointId: 1, type: 'new' }}>
+        <Story />
+      </SubmissionProvider>
+    ),
+  ],
+  parameters: {
+    backgrounds: {
+      default: 'drawer',
+      values: [
+        {
+          name: 'drawer',
+          value: '#4B5563',
+        },
+      ],
+    },
+    xstate: {
+      submission: {
+        events: { type: 'start submission' },
+      },
+    },
+    xstateInspectOptions: {
+      url: 'https://stately.ai/viz?inspect',
+      serialize: null,
+    },
+  },
+};
+
+const Template = (args) => {
+  const data = { ...args };
+
+  return (
+    <div
+      className="relative h-screen overflow-y-auto text-white"
+      style={{ width: '450px', maxWidth: '450px' }}
+    >
+      <GridCoordinates {...data} />
+    </div>
+  );
+};
+
+export const Step3Grid = Template.bind({});
