a60a19b5c4c198a198d6e7bb59da15ac6e9c5b9b
steveoh <sgourley@utah.gov>
2022-11-28 18:40:03 -0700

    style: cards take up the full width

--- src/components/formElements/Card.jsx
+++ src/components/formElements/Card.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 export default function Card({ children }) {
   return (
-    <section className="inline-grid gap-2 rounded-lg border border-slate-400 bg-white p-4 text-sm shadow-md">
+    <section className="inline-grid w-full gap-2 rounded-lg border border-slate-400 bg-white p-4 text-sm shadow-md">
       {children}
     </section>
   );
