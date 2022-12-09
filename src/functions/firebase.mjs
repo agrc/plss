55d791576aee3bb68688d32a41c5bf430335a3de
steveoh <sgourley@utah.gov>
2022-12-08 18:36:09 -0700

    chore: remove try catch

--- src/functions/firebase.mjs
+++ src/functions/firebase.mjs
@@ -1,19 +1,15 @@
 import { initializeApp } from 'firebase-admin/app';
 
 const setupFirebase = () => {
-  try {
-    initializeApp({
-      apiKey: process.env.VITE_FIREBASE_API_KEY,
-      authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.VITE_FIREBASE_DATABASE_URL,
-      projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.VITE_FIREBASE_APPID,
-    });
-  } catch (error) {
-    console.error({ error });
-  }
+  initializeApp({
+    apiKey: process.env.VITE_FIREBASE_API_KEY,
+    authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VITE_FIREBASE_APPID,
+  });
 };
 
 export default setupFirebase;
