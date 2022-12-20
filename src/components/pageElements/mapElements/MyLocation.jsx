b2aeacdd17a2e345993ab141c851e760f9c083be
steveoh <sgourley@utah.gov>
2022-12-19 22:27:54 -0700

    style: update tracking style for gps

--- src/components/pageElements/mapElements/MyLocation.jsx
+++ src/components/pageElements/mapElements/MyLocation.jsx
@@ -48,9 +48,11 @@ MyLocation.propTypes = {
 };
 
 export const GpsButton = forwardRef(({ state, send }, ref) => {
-  console.log('geolocation', state.value, state.context);
   return (
-    <div className="rounded-full bg-white shadow-sm" ref={ref}>
+    <div
+      className="relative flex h-8 w-8 rounded-full bg-white shadow-sm"
+      ref={ref}
+    >
       <button
         onClick={(event) => {
           event.stopPropagation();
@@ -73,20 +75,29 @@ export const GpsButton = forwardRef(({ state, send }, ref) => {
           console.log('geolocation', 'requesting location');
           send('LOCATION REQUESTED');
         }}
-        className={clsx('rounded-full p-2', {
-          'cursor-pointer bg-white': state.matches('gps.idle'),
-          'cursor-not-allowed bg-slate-300': state.matches('notSupported'),
-          'cursor-progress bg-sky-400': state.matches('gps.pending'),
-          'cursor-pointer bg-slate-800': state.matches('resolved'),
-          'cursor-pointer bg-red-700': state.matches('rejected'),
-        })}
+        className={clsx(
+          'flex flex-1 items-center justify-center rounded-full',
+          {
+            'cursor-pointer bg-white':
+              state.matches('gps.idle') || state.matches('resolved'),
+            'cursor-not-allowed bg-slate-300': state.matches('notSupported'),
+            'cursor-progress bg-sky-400': state.matches('gps.pending'),
+            'cursor-pointer bg-red-700': state.matches('rejected'),
+          }
+        )}
       >
+        {state.matches('resolved') && (
+          <span className="absolute top-0 right-0 flex h-3 w-3">
+            <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-amber-300/75"></span>
+            <span className="relative inline-flex h-3 w-3 rounded-full border-2 border-white bg-sky-600/80"></span>
+          </span>
+        )}
         <ViewfinderCircleIcon
           className={clsx('h-6 w-6', {
-            'text-slate-800': state.matches('gps.idle'),
-            'text-slate-700': state.matches('notSupported'),
+            'text-slate-700':
+              state.matches('gps.idle') || state.matches('resolved'),
+            'text-slate-300': state.matches('notSupported'),
             'text-white motion-safe:animate-spin': state.matches('gps.pending'),
-            'text-sky-400 motion-safe:animate-pulse': state.matches('resolved'),
             'text-white': state.matches('rejected'),
           })}
         />
