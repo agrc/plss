a36d938dc98e5623edd5bf0a3bf3f049eabcd6c6
steveoh <sgourley@utah.gov>
2022-12-27 21:14:39 -0700

    style: update classes to allow centering content

--- src/components/formElements/Form.jsx
+++ src/components/formElements/Form.jsx
@@ -20,11 +20,11 @@ NumberedForm.propTypes = {
 export function NumberedFormSection({ children, number, title }) {
   return (
     <>
-      <div className="relative flex w-full items-center font-semibold">
+      <div className="relative flex items-center font-semibold">
         <div className="absolute left-0 top-1/2 z-10 flex h-10 w-10 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full border-2 border-sky-100 bg-white text-sky-500">
           {number > 0 ? number : '👍'}
         </div>
-        <div className="ml-8 uppercase">{title ? title : children}</div>
+        <div className="ml-8 flex-1 uppercase">{title ? title : children}</div>
       </div>
       {title && <div className="ml-8 mb-4 flex flex-col gap-4">{children}</div>}
     </>
