1a99a26847ddfe5655a052a1da442dafe02807a5
steveoh <sgourley@utah.gov>
2023-01-17 19:44:03 -0700

    refactor: use cross env to set local and always use firebase_config

--- src/functions/firebase.mjs
+++ src/functions/firebase.mjs
@@ -1,21 +1,10 @@
 import { initializeApp } from 'firebase-admin/app';
 
 const setupFirebase = () => {
-  let app = {
-    apiKey: process.env.VITE_FIREBASE_API_KEY,
-    authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.VITE_FIREBASE_DATABASE_URL,
-    projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.VITE_FIREBASE_APPID,
-  };
+  const app = JSON.parse(process.env.FIREBASE_CONFIG);
 
-  if (
-    'FIREBASE_CONFIG' in process.env &&
-    process.env.NODE_ENV === 'production'
-  ) {
-    app = JSON.parse(process.env.FIREBASE_CONFIG);
+  if (process.env.NODE_ENV === 'local') {
+    app.storageBucket = 'localhost';
   }
 
   try {
