49bb2d7f3dab237d97ec1cd95c2854c8efac5d12
steveoh <sgourley@utah.gov>
2024-04-04 14:10:08 -0700

    chore: storybook migrations

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
@@ -8,9 +8,6 @@ module.exports = {
   framework: {
     name: '@storybook/react-vite',
     options: {},
-  },
-  features: {
-    storyStoreV7: true,
   },
   docs: {
     autodocs: true,
