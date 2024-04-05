badbf2701ea2bb0f4d6d0b0795ecea70e43c4bc6
steveoh <sgourley@utah.gov>
2024-04-05 10:02:42 -0700

    chore: xstate 5 migration

--- src/components/pageElements/mapElements/useGeoLocation.js
+++ src/components/pageElements/mapElements/useGeoLocation.js
@@ -1,5 +1,5 @@
 import { useMachine } from '@xstate/react';
-import geolocationMachine from '../../machines/geolocation';
+import { machine as geolocationMachine } from '../../machines/geolocation';
 
 export default function useGeolocation() {
   const [state, send] = useMachine(geolocationMachine);
