2822ca1ffe28d641f9018154cf58702648ab4618
steveoh <sgourley@utah.gov>
2024-04-08 13:32:51 -0700

    chore: allow for setting custom context

--- src/components/pageElements/CornerSubmission/0-CornerSubmission.stories.jsx
+++ src/components/pageElements/CornerSubmission/0-CornerSubmission.stories.jsx
@@ -1,10 +1,67 @@
 import CornerSubmission from './CornerSubmission.jsx';
 import { SubmissionProvider } from '../../contexts/SubmissionContext.jsx';
+import { getAnalytics } from 'firebase/analytics';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFunctions } from 'firebase/functions';
+import { getStorage } from 'firebase/storage';
+import {
+  AnalyticsProvider,
+  AuthProvider,
+  FirebaseAppProvider,
+  FunctionsProvider,
+  StorageProvider,
+} from 'reactfire';
 
 export default {
   title: 'Corner/Submission',
   component: CornerSubmission,
-  decorators: [(Story) => <SubmissionProvider>{Story()}</SubmissionProvider>],
+  decorators: [
+    (Story) => {
+      const config = {
+        apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+        authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+        databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+        projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_FIREBASE_APPID,
+      };
+      const app = initializeApp(config);
+      const storage = getStorage(app);
+      const functions = getFunctions(app);
+      const auth = getAuth(app);
+
+      return (
+        <QueryClientProvider client={new QueryClient()}>
+          <FirebaseAppProvider firebaseConfig={config}>
+            <AuthProvider sdk={auth}>
+              <FunctionsProvider sdk={functions}>
+                <StorageProvider sdk={storage}>
+                  <AnalyticsProvider sdk={getAnalytics(app)}>
+                    <SubmissionProvider
+                      context={{
+                        section: 1,
+                        corner: 'NW',
+                        status: 'new',
+                        collected: '2022-01-01',
+                        accuracy: 'surveyor',
+                        description: 'test',
+                        notes: 'notes',
+                      }}
+                    >
+                      {Story()}
+                    </SubmissionProvider>
+                  </AnalyticsProvider>
+                </StorageProvider>
+              </FunctionsProvider>
+            </AuthProvider>
+          </FirebaseAppProvider>
+        </QueryClientProvider>
+      );
+    },
+  ],
   parameters: {
     backgrounds: {
       default: 'drawer',
--- src/components/pageElements/CornerSubmission/1-Metadata.stories.jsx
+++ src/components/pageElements/CornerSubmission/1-Metadata.stories.jsx
@@ -1,15 +1,52 @@
 import Metadata from './Metadata.jsx';
 import { SubmissionProvider } from '../../contexts/SubmissionContext.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFunctions } from 'firebase/functions';
+import { getStorage } from 'firebase/storage';
+import {
+  AuthProvider,
+  FirebaseAppProvider,
+  FunctionsProvider,
+  StorageProvider,
+} from 'reactfire';
 
 export default {
   title: 'Corner/Submission/Parts',
   component: Metadata,
   decorators: [
-    (Story) => (
-      <SubmissionProvider context={{ blmPointId: 1, type: 'new' }}>
-        {Story()}
-      </SubmissionProvider>
-    ),
+    (Story) => {
+      const config = {
+        apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+        authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+        databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+        projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_FIREBASE_APPID,
+      };
+      const app = initializeApp(config);
+      const storage = getStorage(app);
+      const functions = getFunctions(app);
+      const auth = getAuth(app);
+
+      return (
+        <QueryClientProvider client={new QueryClient()}>
+          <FirebaseAppProvider firebaseConfig={config}>
+            <AuthProvider sdk={auth}>
+              <FunctionsProvider sdk={functions}>
+                <StorageProvider sdk={storage}>
+                  <SubmissionProvider context={{ blmPointId: 1, type: 'new' }}>
+                    {Story()}
+                  </SubmissionProvider>
+                </StorageProvider>
+              </FunctionsProvider>
+            </AuthProvider>
+          </FirebaseAppProvider>
+        </QueryClientProvider>
+      );
+    },
   ],
   parameters: {
     backgrounds: {
