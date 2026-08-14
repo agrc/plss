c739e2dd2841686e48634a95079cadb1cad99979
stdavis <stdavis@utah.gov>
2026-08-14 08:58:14 -0600

    chore: remove husky precommit hook
    this is annoying and non-standard

--- commitlint.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'scope-enum': [2, 'always', ['api', 'app']],
-    'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'refactor', 'deps', 'style', 'test']],
-  },
-};
